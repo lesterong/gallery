e18645e7691a3bfa4b4897c6bb5d0da902dda2cb
Lester Ong <lesterong776@gmail.com>
2023-07-10 01:46:50 +0800

    Add character animation for home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,19 @@
 import Page from '../components/Page.tsx';
+import { motion } from 'framer-motion';
 
 const HomePage = () => {
   return (
     <Page>
-      <h1>hello, gallery.</h1>
-      <p>
-        Coming soon.
-      </p>
+      <h1 style={{ display: 'flex' }}>
+        {"hello, gallery.".split("").map((char, index) => {
+          if (char === " ") {
+            return <div key={index}>&nbsp;</div>
+          } else {
+            return <motion.div whileHover={{ y: -8 }} key={index}>{char}</motion.div>;
+          }
+        })}
+      </h1>
+      Coming Soon.
     </Page>
   );
 };
